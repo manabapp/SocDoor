06b951e58f0aa6f6253c02c26cbb22565d34b089
manabapp <manabapp>
2021-02-12 14:45:19 +0900

    Initial Commit

---
+++ SocDoor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SocDoor
+//
+//  Created by Hirose Manabu on 2021/02/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
